095f53bddfbab0351b63c4c1f21357034589da52
phatlenguyen166 <phatlenguyen166@gmail.com>
2024-04-18 21:32:49 +0700

    frist commit

---
+++ pages/footer.php
@@ -0,0 +1,56 @@
+<div class="footer">
+        <div class="footer-content">
+          
+            <div class="colum1">
+                <div class="colum1-title">Danh mục loại sản phẩm</div>
+                <div class="colum1-list">
+
+                        <a href="">Máy tính</a>
+                        <a href="">Máy tính</a>
+                        <a href="">Máy tính</a>
+                        <a href="">Máy tính</a>
+                        
+                    
+                </div>
+            </div>
+            <div class="colum1">
+                <div class="colum1-title">Danh mục thương hiệu</div>
+                <div class="colum1-list">
+                    
+                        <a href="">Máy tính</a>
+                        <a href="">Máy tính</a>
+                        <a href="">Máy tính</a>
+                        <a href="">Máy tính</a>
+                        
+                    
+                </div>
+            </div>
+            <div class="colum1">
+                <div class="colum1-title">Dịch vụ</div>
+                <div class="colum1-list">
+                    
+                        <a href="">Bảo Hành</a>
+                        <a href="">Vận Chuyển</a>
+                        <a href="">Trả Hàng</a>
+                        <a href="">Thanh Toán</a>
+                        <a href="">Tuyển Nhân Viên</a>
+                        
+                    
+                </div>
+            </div>
+            <div class="colum1">
+                <div class="colum1-title">Liên hệ </div>
+                <div class="colum1-list">
+                    
+                        <a href=""><i class="fa-solid fa-phone"></i> 123456789</a>
+                        <a href=""><i class="fa-solid fa-location-dot"></i> 114 Nguyen Phi Khanh Street Tan Dinh Ward District 1, Ho Chi Minh City,Vietnam </a>
+                        <a href=""><i class="fa-solid fa-envelope"></i> vincenttuan1098@gmail.com</a>
+                        
+                        
+                    
+                </div>
+            </div>
+
+        </div>
+        <div class="clear"></div>
+    </div>
